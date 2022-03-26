cf2f075073daf72ed0c6ac9153225641c373c2d4
dream-well <dreamwell506@outlook.com>
2022-03-27 07:03:01 +0800

    fix: bridge operator

--- contracts/JaxBridgeV2.sol
+++ contracts/JaxBridgeV2.sol
@@ -8,11 +8,13 @@ contract JaxBridgeV2 {
 
   uint chainId;
   
+  uint fee = 50;
+
   address public admin;
 
   IERC20 public wjxn = IERC20(0xA25946ec9D37dD826BbE0cbDbb2d79E69834e41e);
 
-  enum RequestStatus {Init, Approved, Rejected, Released}
+  enum RequestStatus {Init, Proved, Rejected, Expired, Released}
 
   struct Request {
     uint amount;
@@ -31,13 +33,16 @@ contract JaxBridgeV2 {
 
   mapping(address => uint[]) public user_requests;
 
+  address[] public bridge_operators;
+  mapping(address => uint) operating_limits;
+
   event Create_Request(
     uint request_id,
     uint amount,
     string from
   );
 
-  event Approve_Request(
+  event Prove_Request(
     uint request_id
   );
 
@@ -89,12 +94,7 @@ contract JaxBridgeV2 {
     requests.push(request);
     uint request_id = requests.length - 1;
     user_requests[to].push(request_id);
-    emit Create_Request(request_id, amount, from);
-  }
 
-  function approve_request(uint request_id) external onlyAdmin {
-    Request storage request = requests[request_id];
-    require(request.status == RequestStatus.Init, "Invalid status");
     uint i = 0;
     for(; i <= deposit_addresses.length; i += 1) {
       if(!is_address_active[i])
@@ -104,27 +104,41 @@ contract JaxBridgeV2 {
     is_address_active[i] = true;
     request.deposit_address_id = i;
     request.valid_until = block.timestamp + 48 hours;
-    request.status == RequestStatus.Approved;
-    emit Approve_Request(request_id);
+    emit Create_Request(request_id, amount, from);
+  }
+
+  function prove_request(uint request_id, string calldata txHash) external {
+    Request storage request = requests[request_id];
+    require(request.status == RequestStatus.Init, "Invalid status");
+    request.txHash = txHash;
+    request.status = RequestStatus.Proved;
+    emit Prove_Request(request_id);
   }
 
   function reject_request(uint request_id) external onlyAdmin {
     Request storage request = requests[request_id];
-    require(request.status == RequestStatus.Init, "Invalid status");
+    // require(request.status == RequestStatus.Init, "Invalid status");
     request.status = RequestStatus.Rejected;
     emit Reject_Request(request_id);
   }
 
   function release(
     uint request_id,
+    uint amount,
+    string calldata from,
+    address to,
     string calldata txHash
-  ) external onlyAdmin {
+  ) external bridgeOperator(amount) {
     Request storage request = requests[request_id];
+    require(request.amount == amount, "amount mismatch");
+    require(keccak256(abi.encodePacked(request.from)) == keccak256(abi.encodePacked(from)), "Sender's address mismatch");
+    require(request.to == to, "destination address mismatch");
+    require(keccak256(abi.encodePacked(request.txHash)) == keccak256(abi.encodePacked(txHash)), "Tx Hash mismatch");
     request.txHash = txHash;
     is_address_active[request.deposit_address_id] = false;
     request.status = RequestStatus.Released;
-    wjxn.transfer(request.to, request.amount - 50);
-    emit Release(request_id, request.to, request.amount - 50);
+    wjxn.transfer(request.to, request.amount - fee);
+    emit Release(request_id, request.to, request.amount - fee);
   }
 
   function get_user_requests(address user) external view returns(uint[] memory) {
@@ -135,4 +149,33 @@ contract JaxBridgeV2 {
       IERC20(token).transfer(msg.sender, amount);
   }
 
+  function add_bridge_operator(address operator, uint operating_limit) external onlyAdmin {
+    for(uint i = 0; i < bridge_operators.length; i += 1) {
+      if(bridge_operators[i] == operator)
+        revert("Already exists");
+    }
+    bridge_operators.push(operator);
+    operating_limits[operator] = operating_limit;
+  }
+
+  function isBridgeOperator(address operator) public view returns(bool) {
+    uint i = 0;
+    for(; i < bridge_operators.length; i += 1) {
+      if(bridge_operators[i] == operator)
+        return true;
+    } 
+    return false;
+  }
+
+  modifier bridgeOperator(uint amount) {
+    require(isBridgeOperator(msg.sender), "Not a bridge operator");
+    require(operating_limits[msg.sender] >= amount, "Amount exceeds operating limit");
+    _;
+    operating_limits[msg.sender] -= amount;
+  }
+
+  function set_operating_limit(address operator, uint operating_limit) external onlyAdmin {
+    require(isBridgeOperator(msg.sender), "Not a bridge operator");
+    operating_limits[operator] = operating_limit;
+  }
 }
